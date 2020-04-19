e3a4de0d4734a09e68885609a8d6f057d4e36636
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-04-19 09:05:20 -0400

    adds mongo db schema and seed

---
+++ models/Book.js
@@ -0,0 +1,38 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+if (mongoose.connection.readyState === 0) {
+  mongoose.connect(require('./connection-string'), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: true
+  });
+}
+
+
+var newSchema = new Schema({
+
+  'title': { type: String },
+  'authors': { type: Array },
+  'description': { type: String },
+  'image': { type: String },
+  'link': { type: String },
+  'createdAt': { type: Date, default: Date.now },
+  'updatedAt': { type: Date, default: Date.now }
+});
+
+newSchema.pre('save', function (next) {
+  this.updatedAt = Date.now();
+  next();
+});
+
+newSchema.pre('update', function () {
+  this.update({}, { $set: { updatedAt: Date.now() } });
+});
+
+newSchema.pre('findOneAndUpdate', function () {
+  this.update({}, { $set: { updatedAt: Date.now() } });
+});
+
+
+
+module.exports = mongoose.model('Book', newSchema);
