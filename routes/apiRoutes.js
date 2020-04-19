2fcd7082b097563fe7ec7ff3b93d41ddadeff318
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-04-19 00:15:20 -0400

    init commit

---
+++ routes/apiRoutes.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+// root directory == /api
+router.get('/', function (req, res, next) {
+  res.json({ success: true, kevin: "is so good at this" });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const htmlRouter = require('./routes/htmlRoutes');
+const apiRouter = require('./routes/apiRoutes');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'client')));
+
+app.use('/', htmlRouter);
+app.use('/api', apiRouter);
+
+module.exports = app;
