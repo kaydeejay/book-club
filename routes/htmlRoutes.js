96863f3b775faced65c337602d4b670be8dac515
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-04-20 07:41:02 -0400

    cleans up the server - time for front-end

--- routes/htmlRoutes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.json({ success: true, secretKey: "should go here" });
-});
-
-module.exports = router;
--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const htmlRouter = require('./routes/htmlRoutes');
 const apiRouter = require('./routes/apiRoutes');
 
 const app = express();
@@ -15,7 +14,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '/client/public')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookClub", {
   useNewUrlParser: true,
@@ -23,7 +24,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookClub", {
   useUnifiedTopology: true
 });
 
-app.use('/', htmlRouter);
 app.use('/api', apiRouter);
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 module.exports = app;
