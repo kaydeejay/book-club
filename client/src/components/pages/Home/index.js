5e4bfbb24e71fe99272f397e73295a3e0ef234f5
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-04-23 17:26:30 -0400

    adds BookList component to homepage

--- client/src/components/pages/Home/index.js
+++ client/src/components/pages/Home/index.js
@@ -1,29 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import BookList from '../../BookList';
-import SearchForm from '../../SearchForm';
-// import SearchBooks from '../../SearchBooks';
+
 
 import API from '../../../utils/API';
 
 const Home = () => {
+  const [appState, setAppState] = useState("ready");
   const [savedBooks, setSavedBooks] = useState([]);
-
-  const loadBooks = () => {
-    API.getBooks()
-      .then(res => {
-        // console.log(res.data);
-        setSavedBooks(res.data);
-      })
-  }
+  // const [searchedBooks, setSearchedBooks] = useState([]);
+  // const [form, setForm] = useState({});
 
   useEffect(() => {
     loadBooks();
   }, [])
 
+  const loadBooks = () => {
+    setAppState("loading");
+    API.getBooks()
+      .then(res => {
+        setSavedBooks(res.data);
+        setAppState("resolved");
+      })
+      .catch(err => {
+        console.log(err);
+        setAppState("failure");
+      });
+  }
+
   return (
     <div>
-      <BookList books={savedBooks} />
-      <SearchForm />
+      {/* BookList, contains BookListItems.
+        These will be reusable for the searched books */}
+      <BookList appstate={appState} header={"Your Library"}>
+        {!savedBooks.length
+          ? props.appstate
+          : savedBooks.map(book => (
+            <BookListItem
+              key={book._id}
+              image={book.image}
+              title={book.title}
+              authors={book.authors.join(', ')}
+              description={book.description.slice(0, 280)}
+            />
+          ))}
+      </BookList>
+      {/* SearchForm, sets searchedBooks */}
+      {/* BookList again, for searched books. */}
     </div>
   )
 }
